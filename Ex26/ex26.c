33d28e1fb714688466f467c2131770d439bebe02
Ruffman <Haucke.Raphael@protonmail.com>
2019-08-14 16:44:46 +0200

    finished logfind program; able to search /var/log-filenames for words

--- Ex26/ex26.c
+++ Ex26/ex26.c
@@ -1,34 +1,105 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
+#include <dirent.h>
 #include "dbg.h"
+
+#define MAX_LINE_SIZE 256
+#define MAX_NUM_LOGTYPES 128
+
+enum mode {
+	AND,
+	OR
+};
+
+void die(char* text)
+{
+	printf("%s\n", text);
+	exit(1);
+}
+
+int checkLognameForWords(char* curFileName, enum mode MODE, int argc, char* argv[])
+{
+	int wordsInName = 0;
+	int word = 0;
+	if (MODE == OR) {
+		word = 2;
+	} else {
+		word = 1;
+	}
+	for ( ; word < argc; ++word) {
+		if(strstr(curFileName, argv[word]) != NULL) {
+			++wordsInName;
+		}
+		if(MODE == OR && wordsInName >= 1) {
+			return 1;
+		}
+	}
+	
+	if (MODE == AND && wordsInName == (argc - 1)) {
+		return 1;
+	}
+	
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
-	bool MODE_AND = true;
+	enum mode MODE = AND;
 	
 	for (int i = 1; i < argc; ++i) {
 		if (argv[i][0] == '-') {
 			if (argv[i][1] == 'o') {
-				MODE_AND = false;
+				if (i > 1) {
+					die("Please put -o as the second argument");
+				}
+				MODE = OR;
 			}
 		}
 	}
 	
-	char* logFind = "~/.logfind";
+	if (argc < 2) {
+		die("You should enter a word to search for");
+	} else if (MODE == OR && argc < 3) {
+		die("You should enter a word to search for");
+	}
+	
+	char* logFind = "/home/ruffman/.logfind";
 	FILE* allowed = fopen(logFind, "r");
-	char* line = malloc(256);
-	// char* allowedFiles[];
+	if (!allowed) {
+		die("Couldn't open file specifying allowed lognames");
+	}
+	char* line = malloc(MAX_LINE_SIZE);
+	char* allowedLogs[MAX_NUM_LOGTYPES];
 	
+	int i = 0;
 	while (fgets(line, sizeof(line), allowed)) {
-		printf("%s", line);
+		allowedLogs[i] = line;
+		++i;
 	}
 	
-	if (MODE_AND) {
-		
+	struct dirent* de = NULL;
+	char* curFileName = NULL;
+	
+	DIR* curLoggingDir = opendir("/var/log");
+	if (!curLoggingDir) {
+		die("Couldn't open directory of log files");
 	}
+	
+	int matchCount = 0;
+	while ((de = readdir(curLoggingDir)) != NULL) {
+		curFileName = de->d_name;
+		
+		int lc = checkLognameForWords(curFileName, MODE, argc, argv);
+		
+		if (lc == 1) {
+			++matchCount;
+			printf("%s\n", curFileName);
+		}
+	}
+	printf("Found %d matching files\n", matchCount);
 	
 	fclose(allowed);
+	closedir(curLoggingDir);
 	free(line);
 	
 	return 0;
